9db95b26c1a8b96ed459d3c1269205ad3d272bd5
Nicholas Gao <nicholas.gao96@gmail.com>
2018-03-28 12:17:53 +0200

    added rms as backup to pixel perfect

--- Backend/vst_reader.py
+++ Backend/vst_reader.py
@@ -1,10 +1,12 @@
 import screen_capture
 from PIL import ImageChops, Image
+import numpy as np
 import time
 import os
 import win32gui
 import win32api
 import win32con
+import math
 
 class VstReader:
     def __init__(self, key_dir):
@@ -21,10 +23,18 @@ class VstReader:
         with self.capture:
             img = self.capture.capture()
             key_img = get_key_image(img)
+            lowest_err = -1
+            lowest_key_err = -1
             for key, im in self.images.items():
                 if check_equality(im, key_img):
-                    return key, key_img
-            return None, key_img
+                    return key, 1, key_img
+                error = get_rms(key_img, im)
+                if error < lowest_err or lowest_err == -1:
+                    lowest_err = error
+                    lowest_key_err = key
+            if lowest_err > 0.1:
+                return None, 1, key_img
+            return lowest_key_err, lowest_err, key_img
 
     def reset(self):
         win32gui.SetForegroundWindow(self.capture.handle)
@@ -53,14 +63,21 @@ def get_key_image(img):
 def check_equality(im1, im2):
     return ImageChops.difference(im1, im2).getbbox() is None
 
+def get_rms(im1, im2):
+    diff = np.asarray(ImageChops.difference(im1, im2)) / 255
+    return math.sqrt(np.mean(np.square(diff)))
+
 if __name__ == '__main__':
     while True:
-        key, key_img = VstReader('./Keys').get_key()
+        start = time.time()
+        key, error, key_img = VstReader('./Keys').get_key()
         if key == None:
             key_img.show()
             key = input('Key: ')
             key_img.save('Keys/%s.png' % key)
-        print(key)
+        print('%s %f' % (key, error))
+        time.sleep(1/5)
+
 
 # if __name__ == '__main__':
 #     with screen_capture.ScreenCapture(".*VSTHost.*") as cap:
